ef14326ac92e6480d21ef94d9da300e83f1fbd4d
larevolucia <reis.lucia@gmail.com>
2024-07-15 14:31:30 +0200

    fixed pylint warning

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,11 +1,15 @@
+"""Hangman game"""
+
 #!/usr/bin/env python3
 
 import random
+
 
 def select_word():
     """Select a random word from a list of possible words."""
     words = ["parachute", "trailblazer", "sparrow", "canine", "freight", "patronize"]
     return random.choice(words)
+
 
 def initialize_game(word):
     """Initialize game variables."""
@@ -13,6 +17,7 @@ def initialize_game(word):
     guess_progress = "*" * len(word)
     guessed_letters = []
     return guess_left, guess_progress, guessed_letters
+
 
 def get_guess(guessed_letters):
     """Get a valid guess from the player."""
@@ -26,47 +31,51 @@ def get_guess(guessed_letters):
         else:
             print("Please enter a single alphabetic character.")
 
-def update_progress(selected_word, guess_progress, key):
+
+def update_progress(word, guess_progress, key):
     """Update the guess progress with the guessed letter."""
     new_guess_progress = list(guess_progress)
-    for i, letter in enumerate(selected_word):
+    for i, letter in enumerate(word):
         if letter == key:
             new_guess_progress[i] = key
     return "".join(new_guess_progress)
 
-def check_game_over(selected_word, guess_progress, guess_left):
+
+def check_game_over(word, guess_progress, guess_left):
     """Check if the game is over (win or lose)."""
     if "*" not in guess_progress:
-        print("You win! The word was: " + selected_word)
+        print("You win! The word was: " + word)
         return True
     if guess_left == 0:
-        print("You lose! The word was " + selected_word)
+        print("You lose! The word was " + word)
         return True
     return False
 
-def start_game(selected_word):
+
+def start_game(word):
     """Start and manage the game."""
-    guess_left, guess_progress, guessed_letters = initialize_game(selected_word)
+    guess_left, guess_progress, guessed_letters = initialize_game(word)
     print(f"You have {guess_left} guesses to figure out the word.")
-    
+
     while True:
         print("Current progress: " + guess_progress)
         key = get_guess(guessed_letters)
         guessed_letters.append(key)
 
-        if key in selected_word:
+        if key in word:
             print("The word contains " + key)
-            guess_progress = update_progress(selected_word, guess_progress, key)
+            guess_progress = update_progress(word, guess_progress, key)
         else:
             print("The word doesn't contain the letter " + key)
             guess_left -= 1
-        
+
         guessed_letters_list = ", ".join(guessed_letters)
         print(f"Guessed letters: {guessed_letters_list}")
         print(f"Guesses left: {guess_left}")
-        
-        if check_game_over(selected_word, guess_progress, guess_left):
+
+        if check_game_over(word, guess_progress, guess_left):
             break
+
 
 if __name__ == "__main__":
     selected_word = select_word()
